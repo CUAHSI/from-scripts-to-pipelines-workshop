b59b55c9057a56669c585af045ab544e942a26a5
Abner Bogan <abnerbog@buffalo.edu>
2025-07-22 12:13:00 -0400

    Use dplyr method for xwalk and joining operations

--- 01_fetch/src/download_nws_data.R
+++ 01_fetch/src/download_nws_data.R
@@ -5,17 +5,25 @@ download_nws_data <- function(nws_conversion, sites){
                      select(nws_conversion, NWS, USGS), 
                      by = c("site_no"="USGS"))
   
+  # Setup an empty table to build
+  flood_stage_xwalk <- tibble()
+  # For each site, capture the flood stage and units
   for(i in sites$NWS){
     
     url <- paste0("https://api.water.noaa.gov/nwps/v1/gauges/",i)
-
+    
     response_data <- request(url) %>%
       req_perform() %>%
       resp_body_json()
-
-    sites$flood_stage[which(sites$NWS %in% i)] <- response_data$flood$categories$minor$stage
-    sites$stage_units[which(sites$NWS %in% i)] <- response_data$flood$stageUnits
-
+    
+    flood_stage_xwalk <- flood_stage_xwalk %>% 
+      bind_rows(tibble(NWS = i,
+                       flood_stage = response_data$flood$categories$minor$stage,
+                       stage_units = response_data$flood$stageUnits))
   }
+  # Join the flood information with the site data
+  sites <- sites %>% 
+    left_join(flood_stage_xwalk, by = 'NWS')
+  
   return(sites)
 }
