bf5227607b965717745d5f1e6cf4fa388c4b4ac7
Abner Bogan <abnerbog@buffalo.edu>
2025-07-22 11:49:06 -0400

    Removing unused code

--- 01_fetch/src/download_nhc_best_track.R
+++ 01_fetch/src/download_nhc_best_track.R
@@ -1,9 +1,5 @@
 download_nhc_best_track <- function(storm_id){
   
-  # ocean <- "al"
-  # storm_num <- "09"
-  # year <- "2017"
-  
   nhc_url <- "http://www.nhc.noaa.gov/gis/best_track/%s_best_track.zip"
   download_url <- sprintf(nhc_url,storm_id)
   
@@ -12,11 +8,6 @@ download_nhc_best_track <- function(storm_id){
   
   # Download the file
   download.file(download_url, destfile = zip_path, mode = "wb")
-  
-  # # Extract the zip file
-  # extracted_files <- unzip(zip_path, exdir = '01_fetch/tmp')
-  # 
-  # request(download_url)
   
   return(zip_path)
   
