e6f21f680485d1431f69ba4ad0d6830a56677c46
Abner Bogan <abnerbog@buffalo.edu>
2025-07-22 15:45:29 -0400

    Missed lumping in this change to previous commit

--- 02_process/src/read_nwis_site_info.R
+++
@@ -1,8 +0,0 @@
-read_nwis_site_info <- function(site_data, out_file){
-  
-  # readNWISsite is from the dataRetrieval package
-  site_no <- unique(site_data$site_no)
-  site_info <- readNWISsite(site_no)
-  write_csv(site_info, out_file)
-  return(site_info)
-}
